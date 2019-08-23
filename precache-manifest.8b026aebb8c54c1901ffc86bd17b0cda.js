6563aed4e7a7e2bc466a4a56ff892310cff86fa9
botezhou <botezhou@sina.com>
2019-08-23 16:10:14 +0800

    react

---
+++ precache-manifest.8b026aebb8c54c1901ffc86bd17b0cda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3cfac2514bfec29e7564086f655c1f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23e6ee6d1d01062212b0",
+    "url": "/static/css/2.84fa6472.chunk.css"
+  },
+  {
+    "revision": "65973e4116ab664acde4",
+    "url": "/static/css/main.8e71267d.chunk.css"
+  },
+  {
+    "revision": "23e6ee6d1d01062212b0",
+    "url": "/static/js/2.62183a2c.chunk.js"
+  },
+  {
+    "revision": "65973e4116ab664acde4",
+    "url": "/static/js/main.456e71bf.chunk.js"
+  },
+  {
+    "revision": "95731fe6e5570d7ea931",
+    "url": "/static/js/runtime~main.33690292.js"
+  },
+  {
+    "revision": "117b1526a7fe0b961a2f3ae957b37198",
+    "url": "/static/media/face.117b1526.jpg"
+  }
+]);
